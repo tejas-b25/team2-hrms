369d6ae58a978a74dbe03822194b55ef557bb848
Tejas Bhandirge <tbhandirge@gmail.com>
2025-08-01 16:55:45 +0530

    Update employee.service.ts

--- frontend/src/app/services/employee.service.ts
+++ frontend/src/app/services/employee.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class EmployeeService {
-  private apiUrl = 'http://localhost:8080/api/employees';
+  private apiUrl = '/api/employees';
 
   constructor(private http: HttpClient) { }
 
