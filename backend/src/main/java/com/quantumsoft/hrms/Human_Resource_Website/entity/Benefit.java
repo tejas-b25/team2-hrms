25156b86352665753cb0e7257df3c50e0341aa38
Gunjan9615 <gunjansherkar@gmail.com>
2025-08-04 18:08:01 +0530

    Update Benefit.java
    Validation apply

--- backend/src/main/java/com/quantumsoft/hrms/Human_Resource_Website/entity/Benefit.java
+++ backend/src/main/java/com/quantumsoft/hrms/Human_Resource_Website/entity/Benefit.java
@@ -3,6 +3,9 @@ package com.quantumsoft.hrms.Human_Resource_Website.entity;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.quantumsoft.hrms.Human_Resource_Website.enums.BenefitType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,13 +25,19 @@ public class Benefit {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long benefitId;
 
+    @NotBlank(message = "Benefit name is required")
+    @Size(max = 100, message = "Benefit name must not exceed 100 characters")
     private String name;
 
+    @NotBlank(message = "Benefit description is required")
+    @Size(max = 255, message = "Description must not exceed 255 characters")
     private String description;
 
+    @NotNull(message = "Benefit type is required")
     @Enumerated(EnumType.STRING)
     private BenefitType type;
 
+    @NotNull(message = "Taxable status is required")
     private Boolean isTaxable;
 
     @CreatedDate
