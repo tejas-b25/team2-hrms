110de863bd07d0a633442cd030a50ab41571ba00
Gunjan9615 <gunjansherkar@gmail.com>
2025-08-04 17:52:48 +0530

    Update AuditLog.java
    Validation apply

--- backend/src/main/java/com/quantumsoft/hrms/Human_Resource_Website/entity/AuditLog.java
+++ backend/src/main/java/com/quantumsoft/hrms/Human_Resource_Website/entity/AuditLog.java
@@ -1,6 +1,8 @@
 package com.quantumsoft.hrms.Human_Resource_Website.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,11 +18,13 @@ public class AuditLog {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Username is required")
     private String username;
 
+    @NotBlank(message = "Action is required")
     private String action;    // LOGIN, LOGOUT, PASSWORD_CHANGE etc.
 
 //    private String ipAddress; // Optional
-
+    @NotNull(message = "Timestamp is required")
     private LocalDateTime timestamp;
 }
