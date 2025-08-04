1f2cff5d3723350ee224e325eb21fee4004b8caa
2612199 <swapnilkanawade@gmail.com>
2025-08-04 17:58:48 +0530

    Update User.java
    Validation apply for user entity

--- backend/src/main/java/com/quantumsoft/hrms/Human_Resource_Website/entity/User.java
+++ backend/src/main/java/com/quantumsoft/hrms/Human_Resource_Website/entity/User.java
@@ -3,6 +3,10 @@ package com.quantumsoft.hrms.Human_Resource_Website.entity;
 import com.quantumsoft.hrms.Human_Resource_Website.enums.Role;
 import com.quantumsoft.hrms.Human_Resource_Website.enums.Status;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.validation.annotation.Validated;
@@ -20,11 +24,20 @@ public class User {
     private Long userID;
 
     @Column(unique = true)
+    @NotBlank(message = "Username is mandatory")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private  String username;
 
     @Column(unique = true, nullable = false)
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email format is invalid")
     private String email;
 
+    @Size(min = 8, message = "Password must be at least 6 characters")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d]).{6,}$",
+            message = "Password must contain at least one uppercase letter, one letter, one digit, and one special character"
+    )
     private  String password;
 
     @Enumerated(EnumType.STRING)
@@ -40,3 +53,5 @@ public class User {
     private LocalDateTime updatedAt;
 
 }
+
+
