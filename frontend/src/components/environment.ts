8b94d3ad5df47ff52fd12df27daa448154ccdc49
Tejas Bhandirge <tbhandirge@gmail.com>
2025-08-04 15:34:08 +0530

    Update environment.ts

--- frontend/src/components/environment.ts
+++ frontend/src/components/environment.ts
@@ -1,12 +1,3 @@
-// export const environment = {
-//   production: false,
-//   apiBaseUrl: 'http://localhost:8080/api', 
-//   appName: 'HRMS',
-//   enableDebug: true
-// };
-
-
-
 export const environment = {
   production: true,
   apiBaseUrl: '/api',
